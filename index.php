3f0e2f33bc2ff930b419f658f6cb28dfb744e7b9
Himal Sarathchandra <himalhp100@gmail.com>
2026-03-10 17:12:52 +0530

    landing page

--- index.php
+++ index.php
@@ -5,39 +5,6 @@ include('includes/config.php');
 if($_SESSION['login']!=''){
 $_SESSION['login']='';
 }
-if(isset($_POST['login']))
-{
-
-$NIC=$_POST['NIC'];
-$password=md5($_POST['password']);
-$sql ="SELECT EmailId,Password,NIC,Status FROM tblstudents WHERE NIC=:NIC and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':NIC', $NIC, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-
-if($query->rowCount() > 0)
-{
- foreach ($results as $result) {
- $_SESSION['NIC']=$result->NIC;
-if($result->Status==1)
-{
-$_SESSION['login']=$result->EmailId;
-echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
-} else {
-echo "<script>alert('Your Account Has been blocked .Please contact admin');</script>";
-
-}
-}
-
-} 
-
-else{
-echo "<script>alert('Invalid Details');</script>";
-}
-}
-
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -53,64 +20,48 @@ echo "<script>alert('Invalid Details');</script>";
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <!-- CUSTOM STYLE  -->
     <link href="assets/css/style.css" rel="stylesheet" />
+	<!-- CUSTOM STYLE  -->
+    <link href="assets/css/cover.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
 </head>
-<body>
-    <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
-<div class="content-wrapper">
-<div class="container">
 
-<div class="row pad-botm">
-<div class="col-md-12">
-<h4 class="header-line">USER LOGIN FORM</h4>
-</div>
-</div>
- <a name="ulogin"></a>            
-<!--LOGIN PANEL START-->           
-<div class="row">
-<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
-<div class="panel panel-info">
-<div class="panel-heading">
- LOGIN FORM
-</div>
-<div class="panel-body">
-<form role="form" method="post">
+  <body style="background-image:url('assets/img/background.jpeg');">
 
-<div class="form-group">
-<label>NIC No.</label>
-<input class="form-control" type="text" name="NIC" required autocomplete="off" />
-</div>
-<div class="form-group">
-<label>Password</label>
-<input class="form-control" type="password" name="password" required autocomplete="off"  />
-<p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
-</div>
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="inner cover">
+
+            <p class="landing-logo">
+              <img src="assets/img/placeholder-lms350x150.png" alt="logo">
+            </p>
+
+            <p class="lead">
+              <a href="login.php" class="btn btn-lg btn-primary">Login</a>
+			        <a href="signup.php" class="btn btn-lg btn-success">Register</a>
+            </p>
+          </div>
 
 
 
- <button type="submit" name="login" class="btn btn-info">LOGIN </button> | <a href="signup.php">Not Register Yet</a>
-</form>
- </div>
-</div>
-</div>
-</div>  
-<!---LOGIN PABNEL END-->            
-             
- 
+        </div>
+
+      </div>
+
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
- <?php include('includes/footer.php');?>
-      <!-- FOOTER SECTION END-->
+
+
     <script src="assets/js/jquery-1.10.2.js"></script>
     <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
       <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
+
 
 </body>
 </html>
